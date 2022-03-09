b9cc0cbcbd4efd264a29dc674dffa99a5ce7f66d
Brian Buchalter <bal711@gmail.com>
2022-03-09 05:50:10 -0700

    data inputs

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  // Events
   const [eventLog, setEventLog] = useState<string[]>([]);
 
   const supportedEvents = [
@@ -18,12 +19,40 @@ function App() {
     <button onClick={recordEvent(event)}>{event}</button>
   ));
 
-  const eventLogEntries = eventLog.map((event) => <li>{event}</li>);
+  // Data
+  const [qtdWagesPaid, setQtdWagesPaid] = useState<number>(0);
+  const [ein, setEin] = useState<string>("");
+  const dataInputs = [
+    <label>
+      QTD Wages Paid:
+      <input
+        type="text"
+        value={qtdWagesPaid}
+        onChange={(e) => setQtdWagesPaid(Number(e.target.value))}
+      />
+    </label>,
+    <label>
+      EIN:
+      <input type="text" value={ein} onChange={(e) => setEin(e.target.value)} />
+    </label>,
+  ];
 
   return (
     <div className="App">
-      {eventButtons}
-      <ul>{eventLogEntries}</ul>
+      <h2>Events</h2>
+      {eventButtons.map((button) => (
+        <p>{button}</p>
+      ))}
+      <h2>Event History</h2>
+      <ul>
+        {eventLog.map((entry) => (
+          <li>{entry}</li>
+        ))}
+      </ul>
+      <h2>Data</h2>
+      {dataInputs.map((input) => (
+        <p>{input}</p>
+      ))}
     </div>
   );
 }
