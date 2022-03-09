d72f6c32c7cc523ed624f1afaac2bd68480c8506
Brian Buchalter <bal711@gmail.com>
2022-03-09 05:27:39 -0700

    Event button, eventLogs without hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {}
+interface AppState {
+  eventLog: string[];
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      eventLog: [],
+    };
+  }
+
+  taxPredictionEvent =
+    "Tax prediction system records an expected tax liability for TWC";
+
+  recordEvent = (event: string) => (e: React.MouseEvent) => {
+    e.preventDefault();
+    this.setState((prevState) => ({
+      eventLog: [...prevState.eventLog, event],
+    }));
+  };
+
+  render() {
+    const eventLogEntries = this.state.eventLog.map((entry) => {
+      return <li>{entry}</li>;
+    });
+
+    return (
+      <div className="App">
+        <button onClick={this.recordEvent(this.taxPredictionEvent)}>
+          {this.taxPredictionEvent}
+        </button>
+        <ul>{eventLogEntries}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
